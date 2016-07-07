14aeb37496824165e95c05f6af59a4edba738224
Jasper Van Der Hoek <jaspervanderhoek@outlook.com>
2016-07-07 14:21:42 -0400

    added dependency list to csv import java

--- javasource/java_library/actions/CSVImporter.java
+++ javasource/java_library/actions/CSVImporter.java
@@ -9,15 +9,11 @@ import java.io.FileOutputStream;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStream;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.poi.hssf.usermodel.HSSFDateUtil;
 
 import replication.MetaInfo;
 import replication.ReplicationSettings;
@@ -40,6 +36,17 @@ import com.mendix.systemwideinterfaces.core.IContext;
 import com.mendix.systemwideinterfaces.core.meta.IMetaPrimitive.PrimitiveType;
 import com.opencsv.CSVParser;
 
+
+/**
+ * 
+ * @author Jasper van der Hoke
+ * 
+ * @dependency  icu4j-49_1.jar
+ * @dependency  opencsv-3.8.jar
+ * @dependency  org.apache.commons-io-2.3.0.jar
+ * @dependency  replication.jar
+ *  
+ */
 public class CSVImporter {
 	private static final ILogNode _logger = Core.getLogger("CSVImporter");
 
@@ -173,48 +180,6 @@ public class CSVImporter {
 		@Override
 		public Object getValueFromDataSet( String column, PrimitiveType type, Object dataSet ) throws ParseException {
 			throw new NotImplementedException();
-		}
-
-		/**
-		 * Given an Excel date with either 1900 or 1904 date windowing, converts it to a java.util.Date.
-		 * 
-		 * NOTE: If the default <code>TimeZone</code> in Java uses Daylight Saving Time then the conversion back to an Excel
-		 * date may not give the same value, that is the comparison
-		 * <CODE>excelDate == getExcelDate(getJavaDate(excelDate,false))</CODE> is not always true. For example if default
-		 * timezone is <code>Europe/Copenhagen</code>, on 2004-03-28 the minute after 01:59 CET is 03:00 CEST, if the excel
-		 * date represents a time between 02:00 and 03:00 then it is converted to past 03:00 summer time
-		 * 
-		 * @param date The Excel date.
-		 * @param use1904windowing true if date uses 1904 windowing, or false if using 1900 date windowing.
-		 * @return Java representation of the date, or null if date is not a valid Excel date
-		 * @see java.util.TimeZone
-		 */
-		@SuppressWarnings("static-access")
-		public Date getJavaDate( double date, boolean use1904windowing ) {
-			if ( !HSSFDateUtil.isValidExcelDate(date) ) {
-				return null;
-			}
-			int wholeDays = (int) Math.floor(date);
-			int millisecondsInDay = (int) ((date - wholeDays) * ((24 * 60 * 60) * 1000L) + 0.5);
-			Calendar calendar = new GregorianCalendar(); // using default time-zone
-			setCalendar(calendar, wholeDays, millisecondsInDay, use1904windowing);
-			return calendar.getTime();
-		}
-
-		public void setCalendar( Calendar calendar, int wholeDays, int millisecondsInDay, boolean use1904windowing ) {
-			int startYear = 1900;
-			int dayAdjust = -1; // Excel thinks 2/29/1900 is a valid date, which it isn't
-			if ( use1904windowing ) {
-				startYear = 1904;
-				dayAdjust = 1; // 1904 date windowing uses 1/2/1904 as the first day
-			}
-			else if ( wholeDays < 61 ) {
-				// Date is prior to 3/1/1900, so adjust because Excel thinks 2/29/1900 exists
-				// If Excel date == 2/29/1900, will become 3/1/1900 in Java representation
-				dayAdjust = 0;
-			}
-			calendar.set(startYear, 0, wholeDays + dayAdjust, 0, 0, 0);
-			calendar.set(Calendar.MILLISECOND, millisecondsInDay);
 		}
 	}
 }
